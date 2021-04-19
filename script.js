9a32804c380a1b7ce6a75f87e1631d1c4bed8518
andreicismas <andrei_cismas@yahoo.it>
2021-04-19 23:51:05 +0200

    add checked

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 var pulsanteCalcola = document.getElementById("button");
 
+// alert("ai vinto un sconto")
+
+// alert("inserisci nells sezzione discount il tuo cupon HTD3I459KSA")
+
 pulsanteCalcola.addEventListener("click", function () {
 
     var nomePanino = document.getElementById("name");
-    var costoPanino = 10;
+
+    var cupon = document.getElementById("coupon");
+    cupon = "HTD3I459KSA"
+    var costoPanino = 50;
+
     var opzioniIngredienti = document.querySelectorAll(".ingredient [type='checkbox']");
     console.log(opzioniIngredienti)
 
     for (var i = 0; i < opzioniIngredienti.length; i++) {
         var ingredienti = opzioniIngredienti[i];
 
-        if (ingredienti.cheched) {
+        if (ingredienti.checked) {
             costoPanino += parseInt(ingredienti.value);
-        }
+        } 
+        
     }
 
- 
+    var prezzoTotale = document.getElementById("price").innerText = costoPanino
 
-
-
-
-    // for (var i = 0; i < opzioniIngredienti.length; i++) {
-    //     if (opzioniIngredienti[i].checked) {
-    //         costoPanino = costoPanino + parseInt(opzioniIngredienti[i].value) + " ";
-    //     }
-    // }
-
+   
 
 
 
