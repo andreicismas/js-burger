58d993900de4435d950a3864536d4970fe3673fa
andreicismas <andrei_cismas@yahoo.it>
2021-04-20 18:37:09 +0200

    add sconto cupon

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+// pulsante genera il prezzo ed eventuale sconto 
 var pulsanteCalcola = document.getElementById("button");
+// nome panino
+var nomePanino = document.getElementById("name");
+// input cupon con eventuale sconto 
+var cupon = document.getElementById("coupon");
+// in discount del cupon pari a15% in questo caso 
+var scontoCupone = 15;
+// un alert con messagiio hai vinto lo sconto 
+alert("ai vinto uno sconto")
 
-// alert("ai vinto un sconto")
-
-// alert("inserisci nells sezzione discount il tuo cupon HTD3I459KSA")
-
+alert("inserisci nells sezzione discount il tuo cupon (jkdsafhfdh6jufds)")
+// lista codici sconto 
+var cuponScount = ["jkdsafhfdh6jufds", "HYT5GF44RDFV",]
 pulsanteCalcola.addEventListener("click", function () {
-
-    var nomePanino = document.getElementById("name");
-
-    var cupon = document.getElementById("coupon");
-    cupon = "HTD3I459KSA"
     var costoPanino = 50;
+
 
     var opzioniIngredienti = document.querySelectorAll(".ingredient [type='checkbox']");
     console.log(opzioniIngredienti)
@@ -20,14 +24,36 @@ pulsanteCalcola.addEventListener("click", function () {
 
         if (ingredienti.checked) {
             costoPanino += parseInt(ingredienti.value);
-        } 
-        
+        }
+
     }
 
-    var prezzoTotale = document.getElementById("price").innerText = costoPanino
+    var codiceInseritoCliente = cupon.value
 
-   
+    var isValidNumber = codiceScontato(codiceInseritoCliente, cuponScount)
 
+    if (isValidNumber) {
+
+        var scontoDaFare = scontoCupone * costoPanino / 100
+
+        costoPanino -= scontoDaFare
+    }
+    
+    document.getElementById("price").innerText = costoPanino
 
 
 });
+
+function codiceScontato(codiceScount, listaCcodici) {
+
+    var codiceInseritoModificato = codiceScount.toLowerCase()
+
+    var codiceValido = false
+
+    if (listaCcodici.indexOf(codiceInseritoModificato) > -1) {
+        codiceValido = true
+    }
+
+    return codiceValido
+
+}
